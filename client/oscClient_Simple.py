e8ad5e35c0429515ffca5ad5b8f1354499d61105
Ryo_Simon <46313038+ryo-simon-mf@users.noreply.github.com>
2020-07-21 21:33:48 +0900

    Update oscClient_Simple.py

--- client/oscClient_Simple.py
+++ client/oscClient_Simple.py
@@ -1,1 +1,36 @@
+import sys
+import argparse
+import random
+import time
+import getch
+from pythonosc import udp_client
 
+def main():
+    arguments = input('Enter Message:')
+    
+    if arguments == 'FIN':
+        print('Finish!!!') 
+        sys.exit()
+    else:
+        client.send_message(args.message, arguments)
+
+        print('')
+        print('IP:', args.ip, 'PORT:', args.port, args.message, arguments)
+        print('')
+        return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', default = '127.0.0.1', help = 'The IP of the OSC sever')
+    parser.add_argument('--port', type = int, default = 2424, help = 'The port of the OSC server is listening on')
+    parser.add_argument('--message', default = '/simon', help = 'The message of the OSC server')
+    args = parser.parse_args()
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    
+    print('IP:', args.ip, 'PORT:', args.port, args.message)
+    print('')
+
+    while True:    
+        main()
+    
